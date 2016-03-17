747413ccf10673f5deec31a8cc5c131c4ec2194a
Linda Mercado <lindacmercado@gmail.com>
2016-03-17 13:24:33 -0600

    update, not connecting to db

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+  var mongoJS = require('mongoJS');
+var app = express();
+var db = mongoJS('birds', ['sightings']);
+var port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//Routes
+app.post('/api/sightings', function(req, res, next) {
+  db.sightings.save(req.body, function(err, result) {
+    if(err) return res.status(500).json(err);
+    else return res.json(result);
+  });
+});
+
+app.get('/api/sightings', function(req, res, next) {
+  var query = {};
+  if(req.query.bird) query.bird = {name: req.query.bird};
+  if(req.query.region) query.region = req.query.region;
+  if(req.query.id) query._id = mongoJS.ObjectId(req.query.id);
+
+  db.sightings.find(query, function(err, result) {
+    if(err) return res.status(500).json(err);
+    else return res.json(result);
+  });
+});
+
+app.put('/api/sightings/:id', function(req, res, next) {
+  if(!req.query.id) return res.status(418).send('request query \'id\' required');
+  db.sightings.findAndModify({
+    query: {_id: mongoJS.ObjectId(req.query.id)},
+    update: {$set: req.body}
+  },
+    function(err, result) {
+      console.log(err, result);
+      if(err) return res.status(500).json(err);
+      else return res.json(result);
+    });
+});
+
+app.delete('/api/sightings/:id', function(req, res, next) {
+  db.sightings.remove({_id: mongoJS.ObjectId(req.query.id)}, function(err, result) {
+    console.log(err, result);
+    if(err) return res.status(500).json(err);
+    else return res.json(result);
+  });
+});
+
+//Connection
+app.listen(port, function() {
+  console.log("Listening on port " + port);
+});
